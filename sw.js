c0d5a1b5a639dd2cbd043a00cc6fddb5d0531389
EMIIA.AI <vstarostin@emiia.ai>
2025-11-11 01:33:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,14 @@
 self.addEventListener('push', event => {
     const options = {
-        body: event.data ? event.data.text() : 'Напоминание: ежедневная проверка SIP',
+        body: event.data ? event.data.text() : 'Напоминание: SIP',
         icon: '/vv.jpg',
         badge: '/vv.jpg',
-        data: { url: 'https://sos.emiia.ai/vv1.html?pwa=true' },
-        actions: [{ action: 'open', title: 'Открыть' }]
+        data: { url: 'https://sos.emiia.ai/vv1.html?pwa=true' }
     };
     event.waitUntil(self.registration.showNotification('EMIIA.AI SIP', options));
 });
 
 self.addEventListener('notificationclick', event => {
     event.notification.close();
-    if (event.action === 'open') {
-        event.waitUntil(clients.openWindow(event.notification.data.url));
-    }
+    event.waitUntil(clients.openWindow(event.notification.data.url));
 });
